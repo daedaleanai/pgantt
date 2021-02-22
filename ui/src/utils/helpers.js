982aefe17f64724dd9182881c7047d8c8e8aacbe
Lukasz Janyst <lukasz@jany.st>
2021-02-22 16:36:17 +0100

    ui: Implement the object comparison helper

---
+++ ui/src/utils/helpers.js
@@ -0,0 +1,38 @@
+//------------------------------------------------------------------------------
+// Copyright (C) 2021 Daedalean AG
+//
+// This file is part of PGantt.
+//
+// PGantt is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// PGantt is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with PGantt.  If not, see <https://www.gnu.org/licenses/>.
+//------------------------------------------------------------------------------
+
+export function objectEquals(x, y) {
+  for(var p in x) {
+    if(x.hasOwnProperty(p)) {
+      if(x[p] !== y[p]) {
+        return false;
+      }
+    }
+  }
+
+  for(var p in y){
+    if(y.hasOwnProperty(p)) {
+      if(x[p] !== y[p]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
